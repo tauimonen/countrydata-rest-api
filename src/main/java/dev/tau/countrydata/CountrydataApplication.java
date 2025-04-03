fc6de54cd5f00eb5bd0cfae7d3cf7d5e99bf5949
tauimonen <tauimonen@gmail.com>
2025-04-03 13:59:57 +0300

    Create a Spring Boot project

---
+++ src/main/java/dev/tau/countrydata/CountrydataApplication.java
@@ -0,0 +1,13 @@
+package dev.tau.countrydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrydataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountrydataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tau/countrydata/CountrydataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tau.countrydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrydataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
