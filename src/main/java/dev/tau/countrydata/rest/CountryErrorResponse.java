435f9ef36cb9076ec3a393d5abc2a5ddc9da0c8c
tauimonen <tauimonen@gmail.com>
2025-04-07 12:48:44 +0300

    Create CountryErrorResponse

---
+++ src/main/java/dev/tau/countrydata/rest/CountryErrorResponse.java
@@ -0,0 +1,41 @@
+package dev.tau.countrydata.rest;
+
+public class CountryErrorResponse {
+
+    private int status;
+    private String message;
+    private Long timeStamp;
+
+    public CountryErrorResponse() {
+    }
+
+    public CountryErrorResponse(int status, String message, Long timeStamp) {
+        this.status = status;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
