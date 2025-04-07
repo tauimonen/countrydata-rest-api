0e07292d7c34094ec056dbd5aa552e107e8387a6
tauimonen <tauimonen@gmail.com>
2025-04-07 12:28:17 +0300

    Create getCountryByCountryCode

--- src/main/java/dev/tau/countrydata/repository/CountryRepository.java
+++ src/main/java/dev/tau/countrydata/repository/CountryRepository.java
@@ -4,5 +4,6 @@ import dev.tau.countrydata.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CountryRepository extends JpaRepository<Country, String> {
+
 }
 
--- src/main/java/dev/tau/countrydata/rest/CountryRestController.java
+++ src/main/java/dev/tau/countrydata/rest/CountryRestController.java
@@ -2,6 +2,7 @@ package dev.tau.countrydata.rest;
 
 import dev.tau.countrydata.entity.Country;
 import dev.tau.countrydata.repository.CountryRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,4 +21,12 @@ public class CountryRestController {
     public List<Country> getAllCountries() {
         return countryRepository.findAll();
     }
+
+    @GetMapping("/countries/{countryCode}")
+    public ResponseEntity<Country> getCountryByCountryCode(@PathVariable String countryCode) {
+        return countryRepository.findById(countryCode)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
